d295325e245461b45c72c92bf375b216bc8aee20
abhishek kadian <abhishekkadiyan@gmail.com>
2015-10-10 11:54:38 +0530

    Implemented costFunction.

--- Machine Learning - Andrew Ng/Week-3/machine-learning-ex2/machine-learning-ex2/ex2/costFunction.m
+++ Machine Learning - Andrew Ng/Week-3/machine-learning-ex2/machine-learning-ex2/ex2/costFunction.m
@@ -7,7 +7,7 @@ function [J, grad] = costFunction(theta, X, y)
 % Initialize some useful values
 m = length(y); % number of training examples
 
-% You need to return the following variables correctly 
+% You need to return the following variables correctly
 J = 0;
 grad = zeros(size(theta));
 
@@ -20,12 +20,13 @@ grad = zeros(size(theta));
 % Note: grad should have the same dimensions as theta
 %
 
+h = sigmoid(X * theta);
 
+t = y' * (log(h));
+u = (1-y)' * (log(1-h));
+J = -(t + u)/m;
 
-
-
-
-
+grad = X' * (h - y);
 
 % =============================================================
 
